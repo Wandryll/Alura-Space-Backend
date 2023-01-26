43005aaf4a92453ae139ea92a75d8845da19d43e
wandryell <wandryell@gmail.com>
2023-01-26 15:18:35 -0400

    fix: database, html scripts

--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
-
+from galeria.models import Fotografia
 # Create your views here.
 
 def index(request):
-    dados = {
-        1: {"nome": "Nebulosa de Carina",
-        "legenda": "webbtelescope.org / NASA / James Webb"},
-        2:{"nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA / Hubble"},
-}
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotograsfias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotograsfias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
