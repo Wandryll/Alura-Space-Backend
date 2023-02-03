31c6bd4f58787757b910793aaaca5f874bf68d35
wandryell <wandryell@gmail.com>
2023-02-02 21:30:23 -0400

    update: databse

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from galeria.models import Fotografia
 
-class ListandoFotografia(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda", "publicada",)#visualizacao do banco de dados
-    list_display_links = ("id", "nome")#colocar como parametro de links
-    search_fields = ("nome",)#adicionar "pesquisar" no django admin
-    list_filter = ("categoria",)#filtrar pesquisa por categorias
-    list_editable = ("publicada",)
-    list_per_page = 10#limitar tamanho de itens por pagina
-
-admin.site.register(Fotografia, ListandoFotografia)
+# Register your models here.
--- galeria/views.py
+++ galeria/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from galeria.models import Fotografia
 # Create your views here.
 
 def index(request):
-    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
-    return render(request, 'galeria/index.html', {"cards": fotografias})
+    fotograsfias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotograsfias})
 
-def imagem(request, foto_id):
-    fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    return render(request, 'galeria/imagem.html', {"fotografia":fotografia})
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
 
