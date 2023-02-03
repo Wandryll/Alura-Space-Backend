960c89f6bcd67d13c3ec3e04602770c6d6221145
wandryell <wandryell@gmail.com>
2023-02-02 23:21:31 -0400

    fix: my mistakes

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "publicada",)#visualizacao do banco de dados
+    list_display_links = ("id", "nome",)#colocar como parametro de links
+    search_fields = ("nome",)#adicionar "pesquisar" no django admin
+    list_filter = ("categoria",)#filtrar pesquisa por categorias
+    list_editable = ("publicada",)
+    list_per_page = 10#limitar tamanho de itens por pagina
+
+admin.site.register(Fotografia, ListandoFotografia)
--- galeria/views.py
+++ galeria/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 # Create your views here.
 
 def index(request):
-    fotograsfias = Fotografia.objects.all()
-    return render(request, 'galeria/index.html', {"cards": fotograsfias})
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia":fotografia})
 
