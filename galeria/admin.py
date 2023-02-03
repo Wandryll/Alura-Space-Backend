d949c521f82f30ffeec098ebba5b9c53ed48735b
wandryell <wandryell@gmail.com>
2023-02-02 21:20:59 -0400

    fix: database, html scripts

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "publicada",)#visualizacao do banco de dados
+    list_display_links = ("id", "nome")#colocar como parametro de links
+    search_fields = ("nome",)#adicionar "pesquisar" no django admin
+    list_filter = ("categoria",)#filtrar pesquisa por categorias
+    list_editable = ("publicada",)
+    list_per_page = 10#limitar tamanho de itens por pagina
+
+admin.site.register(Fotografia, ListandoFotografia)
