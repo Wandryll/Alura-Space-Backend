398e1e65df583e9cf79b7ef87ac175740ac6647d
wandryell <wandryell@gmail.com>
2023-02-09 20:36:59 -0400

    feat: forms, login, cadastro

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,7 +6,7 @@ class ListandoFotografia(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda", "publicada",)#visualizacao do banco de dados
     list_display_links = ("id", "nome",)#colocar como parametro de links
     search_fields = ("nome",)#adicionar "pesquisar" no django admin
-    list_filter = ("categoria",)#filtrar pesquisa por categorias
+    list_filter = ("categoria","usuarios",)#filtrar pesquisa por categorias
     list_editable = ("publicada",)
     list_per_page = 10#limitar tamanho de itens por pagina
 
